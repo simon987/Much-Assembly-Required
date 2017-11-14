42421b7710c0897baa44e696144165fb9e9880d3
simon <Anise5-humid5-Allow-5Lulu-Honk>
2017-11-13 20:08:07 -0500

    Added RCR/RCL Instructions #7.

--- Server/src/net/simon987/server/assembly/instruction/RclInstruction.java
+++ Server/src/net/simon987/server/assembly/instruction/RclInstruction.java
@@ -1,36 +1,43 @@
 package net.simon987.server.assembly.instruction;
 
+
 import net.simon987.server.assembly.Instruction;
 import net.simon987.server.assembly.Status;
 import net.simon987.server.assembly.Target;
 
 /**
- *      +-----------------+
- *      |                 |
+ * +---------------------+
+ * |                     |
  * CF < 0<0<0<0<0<0<0<0 <-+
  */
-public class RolInstruction extends Instruction {
+public class RclInstruction extends Instruction {
 
-    private static final int OPCODE = 35;
+    private static final int OPCODE = 39;
 
-    public RolInstruction() {
-        super("rol", OPCODE);
+    public RclInstruction() {
+        super("rcl", OPCODE);
     }
 
     @Override
     public Status execute(Target dst, int dstIndex, Target src, int srcIndex, Status status) {
 
-        int count = src.get(srcIndex) % 16;
+        int count = src.get(srcIndex) % 17;
 
         int destination = dst.get(dstIndex);
         int signBit = (destination & 0x8000);
 
-        destination = (destination << count) | (destination >>> (16 - count));
+        if (status.isCarryFlag()) {
+            destination |= 0x10000;
+        }
+
+        destination = (destination << count) | (destination >>> (17 - count));
+
+        status.setCarryFlag((destination & 0x10000) == 0x10000);
+
         if (count == 1) {
             status.setOverflowFlag((destination & 0x8000) != signBit); //Set OF if sign bit changed
         }
-        status.setCarryFlag((destination & 1) == 1);
-        dst.set(dstIndex, destination);
+        dst.set(dstIndex, destination & 0xFFFF);
 
 
         return status;
@@ -39,16 +46,23 @@ public class RolInstruction extends Instruction {
     @Override
     public Status execute(Target dst, int dstIndex, int src, Status status) {
 
-        int count = src % 16;
+        int count = src % 17;
+
         int destination = dst.get(dstIndex);
         int signBit = (destination & 0x8000);
 
-        destination = (destination << count) | (destination >>> (16 - count));
+        if (status.isCarryFlag()) {
+            destination |= 0x10000;
+        }
+
+        destination = (destination << count) | (destination >>> (17 - count));
+
+        status.setCarryFlag((destination & 0x10000) == 0x10000);
+
         if (count == 1) {
             status.setOverflowFlag((destination & 0x8000) != signBit); //Set OF if sign bit changed
         }
-        status.setCarryFlag((destination & 1) == 1);
-        dst.set(dstIndex, destination);
+        dst.set(dstIndex, destination & 0xFFFF);
 
 
         return status;
---
+++ Server/src/net/simon987/server/assembly/instruction/RcrInstruction.java
@@ -0,0 +1,69 @@
+package net.simon987.server.assembly.instruction;
+
+import net.simon987.server.assembly.Instruction;
+import net.simon987.server.assembly.Status;
+import net.simon987.server.assembly.Target;
+
+/**
+ * +---------------------+
+ * |                     |
+ * CF < 0<0<0<0<0<0<0<0 <-+
+ */
+public class RcrInstruction extends Instruction {
+
+    private static final int OPCODE = 40;
+
+    public RcrInstruction() {
+        super("rcr", OPCODE);
+    }
+
+    @Override
+    public Status execute(Target dst, int dstIndex, Target src, int srcIndex, Status status) {
+
+        int count = src.get(srcIndex) % 17;
+
+        int destination = dst.get(dstIndex) << 1;
+        int signBit = (destination & 0x10000);
+
+        if (status.isCarryFlag()) {
+            destination |= 1;
+        }
+
+        destination = (destination >>> count) | (destination << (17 - count));
+
+        status.setCarryFlag((destination & 1) == 1);
+
+        if (count == 1) {
+            status.setOverflowFlag((destination & 0x10000) != signBit); //Set OF if sign bit changed
+        }
+        dst.set(dstIndex, destination >> 1);
+
+
+        return status;
+    }
+
+    @Override
+    public Status execute(Target dst, int dstIndex, int src, Status status) {
+
+        int count = src % 17;
+
+        int destination = dst.get(dstIndex) << 1;
+        int signBit = (destination & 0x10000);
+
+        if (status.isCarryFlag()) {
+            destination |= 1;
+        }
+
+        destination = (destination >>> count) | (destination << (17 - count));
+
+        status.setCarryFlag((destination & 1) == 1);
+
+        if (count == 1) {
+            status.setOverflowFlag((destination & 0x10000) != signBit); //Set OF if sign bit changed
+        }
+        dst.set(dstIndex, destination >> 1);
+
+
+        return status;
+    }
+}
