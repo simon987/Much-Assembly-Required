deb3859dffce1c82b858b31787adecabc29954df
Ethan Lafrenais <elafrenais12@gmail.com>
2017-12-31 18:25:46 -0500

    Add PUSHF and POPF instructions

---
+++ Server/src/main/java/net/simon987/server/assembly/instruction/PushfInstruction.java
@@ -0,0 +1,42 @@
+package net.simon987.server.assembly.instruction;
+
+import net.simon987.server.assembly.CPU;
+import net.simon987.server.assembly.Instruction;
+import net.simon987.server.assembly.Register;
+import net.simon987.server.assembly.Status;
+
+/**
+ * Pushes the current CPU flags onto the stack.
+ */
+public class PushfInstruction extends Instruction {
+
+    /**
+     * Opcode of the instruction
+     */
+    public static final int OPCODE = 43;
+
+    private CPU cpu;
+
+    public PushfInstruction(CPU cpu) {
+        super("pushf", OPCODE);
+
+        this.cpu = cpu;
+    }
+
+    @Override
+    public Status execute(Status status) {
+
+        // Decrement SP
+        Register sp = cpu.getRegisterSet().getRegister("SP");
+        sp.setValue(sp.getValue() - 1);
+
+        // Push the current flags
+        cpu.getMemory().set(sp.getValue(), status.toByte());
+
+        return status;
+    }
+
+    public boolean noOperandsValid() {
+        return true;
+    }
+}
