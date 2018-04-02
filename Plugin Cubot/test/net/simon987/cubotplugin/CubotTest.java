9349ae108b84b95503614264585dcde752d2236b
simon987 <fortier.simon@hotmail.com>
2018-04-02 10:58:24 -0400

    Removed some unnecessary unit tests

--- Plugin Cubot/test/net/simon987/cubotplugin/CubotTest.java
+++ Plugin Cubot/test/net/simon987/cubotplugin/CubotTest.java
@@ -3,17 +3,10 @@ package net.simon987.cubotplugin;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-
 public class CubotTest {
 
     @Test
     public void test(){
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        assertEquals(1, 1);
+
     }
 }
