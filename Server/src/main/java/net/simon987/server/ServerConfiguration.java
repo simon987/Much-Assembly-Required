0845438297d742379f5054daf9d5e6dc8f1ce2e2
Simon <fortier.simon@hotmail.com>
2018-10-28 09:45:36 -0400

    Fixed potential error in ServerConfiguration and added ability to programmatically modify server configuration

--- Server/src/main/java/net/simon987/server/ServerConfiguration.java
+++ Server/src/main/java/net/simon987/server/ServerConfiguration.java
@@ -3,29 +3,39 @@ package net.simon987.server;
 
 import net.simon987.server.logging.LogManager;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Properties;
 
 /**
- * Wrapper for Java Property
+ * Wrapper for Java Properties class
  */
 public class ServerConfiguration {
 
-    /**
-     * Properties
-     */
     private Properties properties;
+    private String fileName;
 
-    public ServerConfiguration(String file) {
+    public ServerConfiguration(String fileName) {
+
+        this.fileName = fileName;
+
         try {
             properties = new Properties();
-            InputStream is = new FileInputStream("config.properties");
+            InputStream is = new FileInputStream(this.fileName);
             properties.load(is);
 
         } catch (IOException e) {
             LogManager.LOGGER.severe("Problem loading server configuration: " + e.getMessage());
+        }
+    }
+
+    private void saveConfig() {
+
+        try {
+            OutputStream os = new FileOutputStream(this.fileName);
+            properties.store(os, "");
+
+        } catch (IOException e) {
+            LogManager.LOGGER.severe("Problem saving server configuration: " + e.getMessage());
         }
     }
 
@@ -37,6 +47,16 @@ public class ServerConfiguration {
     public String getString(String key) {
 
         return (String) properties.get(key);
+    }
+
+    public void setInt(String key, int value) {
+        properties.setProperty(key, String.valueOf(value));
+        saveConfig();
+    }
+
+    public void setString(String key, String value) {
+        properties.setProperty(key, value);
+        saveConfig();
     }
 
 }
