ae41bd9cb9e434df03d3d5b5fac3f2cf255c17bf
Simon <fortier.simon@hotmail.com>
2018-11-30 21:51:34 -0500

    Added construction plugin module

---
+++ plugin-contruction/src/main/java/net/simon987/constructionplugin/ConstructionPlugin.java
@@ -0,0 +1,12 @@
+package net.simon987.constructionplugin;
+
+import net.simon987.server.ServerConfiguration;
+import net.simon987.server.game.objects.GameRegistry;
+import net.simon987.server.plugin.ServerPlugin;
+
+public class ConstructionPlugin extends ServerPlugin {
+    @Override
+    public void init(ServerConfiguration config, GameRegistry gameRegistry) {
+
+    }
+}
