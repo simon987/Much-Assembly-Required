d5ddb8e439c197990a4a1cb0b71d84157497b384
Kevin Ramharak <kevin@ramharak.nl>
2019-01-22 20:54:26 +0100

    allow operand validation methods to be overriden

--- Server/src/main/java/net/simon987/server/assembly/Instruction.java
+++ Server/src/main/java/net/simon987/server/assembly/Instruction.java
@@ -102,7 +102,7 @@ public abstract class Instruction {
      * @param o2 Source operand
      * @return true if valid
      */
-    public static boolean operandsValid(Operand o1, Operand o2) throws IllegalOperandException {
+    public boolean operandsValid(Operand o1, Operand o2) {
         return o1.getType() != OperandType.IMMEDIATE16;
     }
 
@@ -112,7 +112,7 @@ public abstract class Instruction {
      * @param o1 source operand
      * @return true if the specified operand can be used with this instruction
      */
-    public static boolean operandValid(Operand o1) {
+    public boolean operandValid(Operand o1) {
         return true;
     }
 
