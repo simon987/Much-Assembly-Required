a3253e8e3af4b8f0aa3b9d84b0f02389cc716973
Kevin Ramharak <kevin@ramharak.nl>
2019-01-23 14:08:40 +0100

    fix setg

--- Server/src/main/java/net/simon987/server/assembly/instruction/SetccInstruction.java
+++ Server/src/main/java/net/simon987/server/assembly/instruction/SetccInstruction.java
@@ -263,7 +263,7 @@ public class SetccInstruction extends Instruction {
      *   SETG, SETNLE       Greater, Not Less or Equal           SF=OF AND ZF=0
      */
     private static Status setg(Target dst, int dstIndex, Status status) {
-        boolean condition = (status.isSignFlag() == status.isOverflowFlag()) && status.isZeroFlag();
+        boolean condition = (status.isSignFlag() == status.isOverflowFlag()) && !status.isZeroFlag();
         int value = condition ? 1 : 0;
         dst.set(dstIndex, value);
         return status;
