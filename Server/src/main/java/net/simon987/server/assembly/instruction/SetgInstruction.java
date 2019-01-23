f3ae97c0607be0ef5f8da45354dd5c672da62c0b
Kevin Ramharak <kevin@ramharak.nl>
2019-01-23 09:43:33 +0100

    fix typo

--- Server/src/main/java/net/simon987/server/assembly/instruction/SetgInstruction.java
+++ Server/src/main/java/net/simon987/server/assembly/instruction/SetgInstruction.java
@@ -3,9 +3,9 @@ package net.simon987.server.assembly.instruction;
 /**
  * alias of SetccInstruction
  */
-public class SetglInstruction extends SetccInstruction {
+public class SetgInstruction extends SetccInstruction {
 
-    public SetglInstruction() {
-        super("setgl");
+    public SetgInstruction() {
+        super("setg");
     }
 }
