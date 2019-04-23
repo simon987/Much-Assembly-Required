f9c7f028702cb82a0df3516f8cad938514a3549e
Kevin Ramharak <kevin@ramharak.nl>
2019-04-23 21:56:31 +0200

    fix tests and configuration

--- Server/src/test/java/net/simon987/server/assembly/MemoryTest.java
+++ Server/src/test/java/net/simon987/server/assembly/MemoryTest.java
@@ -5,12 +5,29 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
 
 public class MemoryTest {
+    
+    ServerConfiguration getConfig() {
+        String filePath = "config.properties";
+
+        if (!new File(filePath).exists()) {
+            File fallback = new File("Server/src/main/resources/", filePath);
+            if (fallback.exists()) {
+                filePath = fallback.getAbsolutePath();
+            } else {
+                throw new AssertionError("'config.properties' and 'Server/src/main/resources/config.properties' cannot be found with working directory: " + new File("").getAbsolutePath());
+            }
+        }
+
+        ServerConfiguration config = new ServerConfiguration(filePath);
+        return config;
+    }
+
     @Test
     public void getSet() {
-        ServerConfiguration config = new ServerConfiguration("config.properties");
-        int memorySize = config.getInt("memory_size");
+        int memorySize = getConfig().getInt("memory_size");
         Memory memory = new Memory(memorySize);
 
         memory.set(1, 1);
@@ -28,11 +45,8 @@ public class MemoryTest {
 
     @Test
     public void write() {
-
-        ServerConfiguration config = new ServerConfiguration("config.properties");
-        int memorySize = config.getInt("memory_size");
+        int memorySize = getConfig().getInt("memory_size");
         Memory memory = new Memory(memorySize);
-
 
         assertTrue(memory.write(0, new char[memorySize], 0, memorySize));
         assertFalse(memory.write(0, new char[memorySize], 0, memorySize + 1));
