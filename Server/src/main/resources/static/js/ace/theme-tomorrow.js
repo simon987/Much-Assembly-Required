60515fe9829b523eb057f4b3a7967692c4cbb7c6
yesji <55633427+yesji@users.noreply.github.com>
2019-10-11 22:29:50 +0530

    Fixed "Module is defined but never used" issue

--- Server/src/main/resources/static/js/ace/theme-tomorrow.js
+++ Server/src/main/resources/static/js/ace/theme-tomorrow.js
@@ -1,4 +1,4 @@
-define("ace/theme/tomorrow", ["require", "exports", "module", "ace/lib/dom"], function (require, exports, module) {
+define("ace/theme/tomorrow", ["require", "exports", "ace/lib/dom"], function (require, exports) {
 
     exports.isDark = false;
     exports.cssClass = "ace-tomorrow";
