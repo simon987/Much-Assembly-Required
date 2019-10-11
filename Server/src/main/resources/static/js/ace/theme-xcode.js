02a9010ae099c7c9531cf1b3d472733ef6a3ac0c
yesji <55633427+yesji@users.noreply.github.com>
2019-10-11 22:40:09 +0530

    Fixed "Module is defined but never used" issue

--- Server/src/main/resources/static/js/ace/theme-xcode.js
+++ Server/src/main/resources/static/js/ace/theme-xcode.js
@@ -1,4 +1,4 @@
-define("ace/theme/xcode", ["require", "exports", "module", "ace/lib/dom"], function (require, exports, module) {
+define("ace/theme/xcode", ["require", "exports", "ace/lib/dom"], function (require, exports) {
 
     exports.isDark = false;
     exports.cssClass = "ace-xcode";
