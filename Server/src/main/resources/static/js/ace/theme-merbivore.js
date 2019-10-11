98644b1b6e2ffd658fbbd107b58a040d24707445
yesji <55633427+yesji@users.noreply.github.com>
2019-10-11 22:35:33 +0530

    Fixed "Module is defined but never used" issue

--- Server/src/main/resources/static/js/ace/theme-merbivore.js
+++ Server/src/main/resources/static/js/ace/theme-merbivore.js
@@ -1,4 +1,4 @@
-define("ace/theme/merbivore", ["require", "exports", "module", "ace/lib/dom"], function (require, exports, module) {
+define("ace/theme/merbivore", ["require", "exports", "ace/lib/dom"], function (require, exports) {
 
     exports.isDark = true;
     exports.cssClass = "ace-merbivore";
