9705b4edad79879212bb3f970f59b20537761bf1
Palvika-Goyal <54737764+Palvika-Goyal@users.noreply.github.com>
2020-10-21 13:13:11 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# This file is ran during vagrant provision
+
 add-apt-repository ppa:webupd8team/java
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.6 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.6.list
